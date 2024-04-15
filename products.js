85486b976ee35cd2e4493915b9299d4d6f3453ec
ALAMINE <codeloccol@iMac-de-CODELOCCOL-3.local>
2024-04-15 08:47:42 +0100

    tea

---
+++ products.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+        "id": 1,
+        "name":" Thé + Canelle",
+        "price": 300,
+        "image": "images/T1.png"
+    },
+    {
+        "id": 2,
+        "name":" Thé + Dattes",
+        "price": 250,
+        "image": "images/T2.png"
+    },
+    {
+        "id": 3,
+        "name":" Thé Sucrerie",
+        "price": 250,
+        "image": "images/T3.png"
+    },
+    {
+        "id": 4,
+        "name":" Thé + Miel",
+        "price": 200,
+        "image": "images/T4.png"
+    },
+    {
+        "id": 5,
+        "name":"Thé Gironfle",
+        "price": 300,
+        "image": "images/T5.png"
+    },
+    {
+        "id": 6,
+        "name":" Thé Marocain",
+        "price": 400,
+        "image": "images/T6.png"
+    },
+    {
+        "id": 7,
+        "name":" Thé + Café",
+        "price": 500,
+        "image": "images/T7.png"
+    },
+    {
+        "id": 8,
+        "name":" Thé Targui",
+        "price": 500,
+        "image": "images/T8.png"
+    }
+
+];
+export default products;
